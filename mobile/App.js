654ca1779c01913a4904597a09670a20a6ec45a9
sandeep <sandeep.raghuvansi@idealittechno.com>
2026-01-15 15:06:58 +0530

    add splash screen

--- mobile/App.js
+++ mobile/App.js
@@ -224,7 +224,7 @@ const styles = StyleSheet.create({
   splashImage: {
     width: "100%",
     height: "100%",
-    resizeMode: "contain",
+    resizeMode: "cover",
   },
   // ... (rest of your existing styles)
 });
